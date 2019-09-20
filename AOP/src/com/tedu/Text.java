3503b4fafa7920828640a26f154a289dd2632e4f
Administrator <Administrator@MTFIYMKEHEVA516>
2019-09-20 20:11:15 +0800

    6

---
+++ AOP/src/com/tedu/Text.java
@@ -0,0 +1,14 @@
+package com.tedu;
+
+public class Text {
+	public static void main(String[] args) {
+		try {
+			System.out.println("before");
+			System.out.println("afterReturning");
+		} catch (Exception e) {
+			System.out.println("afterThrowing");
+		}finally {
+			System.out.println("after");
+		}
+	}
+}
